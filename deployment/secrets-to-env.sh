08123db6e3a18d8bc630410ec663b8efd80c3c58
Colin Wood <colinjameswood@gmail.com>
2018-05-21 11:15:38 -0500

    Changes project name from service.codex-title-registry-api to service.codex-registry-api

--- deployment/secrets-to-env.sh
+++ deployment/secrets-to-env.sh
@@ -3,7 +3,7 @@
 EC2_AVAILABILITY_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
 EC2_REGION=$(echo $EC2_AVAILABILITY_ZONE | sed 's/[a-z]$//')
 
-aws secretsmanager get-secret-value --secret-id codex-title-registry-api --region $EC2_REGION |
+aws secretsmanager get-secret-value --secret-id codex-registry-api --region $EC2_REGION |
 grep 'SecretString' |
 sed -E $'s/.*\{\\\\"(.*)\\\\"\}.*/\\1/' |
 sed -E $'s/\\\\":\\\\"/=/g' |
