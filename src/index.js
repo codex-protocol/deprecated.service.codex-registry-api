fd681e4398e5560c64287e9e0cfe9d32e1ab2462
Colin Wood <colinjameswood@gmail.com>
2018-06-07 18:10:09 -0500

    Adds basic socket connection stuff

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import 'babel-polyfill'
 
+import http from 'http'
+import io from 'socket.io'
 import express from 'express'
+
 
 import config from './config'
 import logger from './services/logger'
 import initialize from './initializers'
 
 const app = express()
+const httpApp = http.Server(app)
+const socketApp = io(httpApp, { serveClient: false })
 
-initialize(app)
+initialize(app, socketApp)
   .then(() => {
-    const listener = app.listen(config.process.port, () => {
+    const listener = httpApp.listen(config.process.port, () => {
       logger.verbose(`server listening on port ${listener.address().port}`)
     })
   })
