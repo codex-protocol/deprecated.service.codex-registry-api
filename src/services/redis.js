939c1e8d87f6e20cee96a31c7a86d7e2acd2ff41
Colin Wood <colinjameswood@gmail.com>
2018-07-19 12:37:26 -0500

    Adds keyPrefix to all redis keys so multiple production servers can use the same redis cluster without clashing

--- src/services/redis.js
+++ src/services/redis.js
@@ -9,8 +9,10 @@ const client = redis.createClient(config.redis)
 
 export default {
 
+  keyPrefix: `${process.env.ETHEREUM_NETWORK_ID}:`,
+
   get(key) {
-    return client.getAsync(key)
+    return client.getAsync(this.keyPrefix + key)
       .then((result) => {
         if (!result) return undefined
         return JSON.parse(result)
@@ -18,7 +20,7 @@ export default {
   },
 
   set(key, value) {
-    return client.setAsync(key, JSON.stringify(value))
+    return client.setAsync(this.keyPrefix + key, JSON.stringify(value))
   },
 
   // default expiry is 1 day
@@ -26,16 +28,16 @@ export default {
   // @NOTE: The expiry is reset when the key is accessed
   //  see: http://redis.io/commands/expire
   setAndExpire(key, value, seconds = 86400) {
-    return client.setexAsync(key, seconds, JSON.stringify(value))
+    return client.setexAsync(this.keyPrefix + key, seconds, JSON.stringify(value))
   },
 
   remove(key) {
-    return client.delAsync(key)
+    return client.delAsync(this.keyPrefix + key)
   },
 
   // @NOTE: The expiry is reset when the key is accessed
   //  see: http://redis.io/commands/expire
   expire(key, seconds) {
-    return client.expireAsync(key, seconds)
+    return client.expireAsync(this.keyPrefix + key, seconds)
   },
 }
